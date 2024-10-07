001a5d2498c235665f0ebeea01e178deecab2b7a
David Prock <dp@dprockcreative.com>
2024-10-07 12:36:45 -0700

    for new hosting

--- assets/js/ie.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  var ie = typeof navigator !== 'undefined' && navigator.appVersion && navigator.appVersion.match(/MSIE/) || null;
-
-  if (ie) {
-    location.href = 'ie.htm';
-  }
-})();
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.set('port', (process.env.PORT || 3000));
-
 app.use(express.static(__dirname + '/'));
 
 app.get('/', (request, response) => {
-  response.sendFile(__dirname + '/index.htm');
+  response.sendFile(__dirname + '/index.html');
 });
 
+app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
 });
